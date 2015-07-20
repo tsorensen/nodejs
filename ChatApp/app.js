fb534bc68b0f3a8e6a37781a7b1baa7ed9495073
tsorensen <tysonsorensen3@gmail.com>
2015-07-20 15:14:47 -0600

    ability to display messages

--- ChatApp/app.js
+++ ChatApp/app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 app = express(),
 server = require('http').createServer(app),
-io = require('socket.io').listen(server);
+io = require('socket.io').listen(server),
+usernames = [];
 
 server.listen(process.env.PORT || 3000);
 
@@ -10,8 +11,32 @@ app.get('/', function(req, res){
 });	
 
 io.sockets.on('connection', function(socket){
+	socket.on('new user', function(data, callback){
+		//check for usernames
+		if(usernames.indexOf(data) != -1) {
+			callback(false);
+		} else {
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+	//update usernames
+	function updateUsernames() {
+		io.sockets.emit('usernames', usernames);
+	}
+
 	//send message
 	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data});
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
+
+	//disconnect
+	socket.on('disconnect', function(data) {
+		if(!socket.username) return;
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
 	});
 });
